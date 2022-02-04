4dde29e50f9a5d1618ac7401c224962e09b4374a
qzou1215 <zouqt.christine2018@gdhfi.com>
2022-02-04 14:52:31 -0800

    Create SkillDemoTest.java

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTest {
+    @Test
+    public void test() {
+        assertEquals(0, SkillDemo.minus(2, 1));
+    }
+}
